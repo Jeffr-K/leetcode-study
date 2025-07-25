5b8ff2b26d0a232dadd5ee52984f6b48b2803d07
oscar.rs <jeff.cofos@gmail.com>
2025-07-25 20:50:01 +0900

    contains-duplicate solution using rust

---
+++ contains-duplicate/Jeffr-K.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+
+/// Contains Duplicate 
+/// :💬Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+/// :💾Link: https://leetcode.com/problems/contains-duplicate/description/
+/// :⏳TC:O(n)
+/// :💾SC:O(n)
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut set = HashSet::new();
+    
+    for num in nums.iter() {
+        if let false = set.insert(num) {
+          return true
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_contains_duplicate() {
+        let nums1 = vec![1,2,3,1];
+        let nums2 = vec![1,2,3,4];
+        let nums3 = vec![1,1,1,3,3,4,3,2,4,2];
+        assert_eq!(contains_duplicate(nums1), true);
+        assert_eq!(contains_duplicate(nums2), false);
+        assert_eq!(contains_duplicate(nums3), true);
+    }
+}
---
+++ two-sum/Jeffr-K.rs
@@ -0,0 +1,27 @@
+struct Solution;
+
+impl solution {
+  
+  /// TC: 
+  /// SC:
+  fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let m = HashMap::new();
+    if let result = nums.iter().as_mut() {
+      let mut diff = target - nums[i];
+      if !m.contains(diff) {
+        m.put(diff)
+      }
+    } else {
+      continue
+    }
+  }
+
+  m.collect<Vec::i32>
+}
+
+mod tests {
+  
+  fn test_two_sum() {
+    assert_eq!()
+  }
+}
